ccdd4679271a27662577c62d083af2cc23bdbe80
joelpalmer <joel.palmer@demand-energy.com>
2017-04-17 18:34:54 -0500

    fromEvent

--- src/app.js
+++ src/app.js
@@ -1,4 +1,31 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const btn = $('#btn');
+const input = $('#input');
+const btnStream$ = Rx.Observable.fromEvent(btn, 'click');
+btnStream$.subscribe(
+    (e) => {
+        console.log(e);
+    },
+     (err) => {
+        console.log(err);
+    },
+    () => {
+        console.log('Completed');
+    }
+);
+
+const inputStream$ =  Rx.Observable.fromEvent(input, 'keyup');
+inputStream$.subscribe(
+    (e) => {
+        console.log(e);
+    },
+     (err) => {
+        console.log(err);
+    },
+    () => {
+        console.log('Completed');
+    }
+);
+
